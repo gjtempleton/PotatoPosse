2c79bb43aac1bc00776221da040b19a263b76d23
Guy Templeton <guyjtempleton@googlemail.com>
2014-09-23 13:14:56 +0100

    Created HTTPHandler class

---
+++ PotatoPosse/src/com/example/potatoposse/HTTPHandler.java
@@ -0,0 +1,39 @@
+package com.example.potatoposse;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+public class HTTPHandler {
+	HttpClient client = new DefaultHttpClient();
+	String request;
+	HttpResponse response;
+	String responseString;
+	
+	public HTTPHandler(String request){
+		this.request = request;
+		try {
+			response = client.execute(new HttpGet(request));
+			StatusLine status = response.getStatusLine();
+			if(status.getStatusCode()==HttpStatus.SC_OK){
+				ByteArrayOutputStream out = new ByteArrayOutputStream();
+		        response.getEntity().writeTo(out);
+		        out.close();
+		        responseString = out.toString();
+			}
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
